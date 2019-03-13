992b0530e40d48aa142f5f9746f8035bb352f3bf
Greg Flores <floresgreg@hotmail.com>
2019-03-13 12:21:00 -0500

    variable intro

--- hello_world_2.py
+++ hello_world_2.py
@@ -4,4 +4,33 @@ print(message)
 message = "hello Python Crash Course world!"
 print(message)
 
-print("Git Test")
+
+# ! Important
+# //hate you
+# * What was this again?
+# TODO: THIS NEEDS TO BE DONE
+# ? What needed to be done?
+
+#* Naming and using Variables
+# Variable names 
+# Letters numbers and underscores
+# Start with letter or underscore not number
+# No spaces, use underscores instead
+# Do not use python keywords and functions names as var names
+# var names should be short and descriptive
+# name, student_name, name_length
+# n, s_n, length_of_persons_name
+# 0o,lIiL
+# For now avoid uppercase variables
+
+#! Creates an error
+# message = "Hello Python Crash Course reader!"
+# print(mesage)
+# [Running] python -u "c:\Users\GregorioF\Documents\PythonCrashCourse\hello_world_2.py"
+# Hello Python world!
+# hello Python Crash Course world!Traceback (most recent call last):
+#   File "c:\Users\GregorioF\Documents\PythonCrashCourse\hello_world_2.py", line 27, in <module>
+    
+# print(mesage)
+# NameError: name 'mesage' is not defined
+
