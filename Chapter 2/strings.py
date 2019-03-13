3adc5e6b576bca29f0c9c78ee1e052f830d6d720
Greg Flores <floresgreg@hotmail.com>
2019-03-13 13:44:48 -0500

    strings section chap2

---
+++ Chapter 2/strings.py
@@ -0,0 +1,14 @@
+#* Strings
+# A string is a series of characters
+# Anything inside quotes is condifered a string
+# You can use singe' or double" quotes around strings
+
+"This is a string"
+'This is also a string'
+
+#This allows you to use quotes or apostrophes inside your strings
+'I told my friend, "Python is my favorite language!"'
+"The language 'Python' is named after Monty Python, not the snake."
+"One of Python's strengths is its diverse and supportive community."
+
+#*
