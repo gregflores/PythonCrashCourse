95309a69b4a5d994360ab53f5b4bdfc653dafc2a
Greg Flores <floresgreg@hotmail.com>
2019-03-20 21:23:48 -0500

    chp 7

---
+++ Chapter 7/1_parrot.py
@@ -0,0 +1,5 @@
+
+#* Most programs are written to solve an end user's problem. To do so,
+#  you usually need to get some information from the user. For a simple
+#  example, let's say someone wants to find out, whether they're old
+#  enough to vote.
